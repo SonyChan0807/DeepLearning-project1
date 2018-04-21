ca5bf23787a013144fb3ebab81ffa05d9396e9d2
HaziqRazali <ha_haziq_91@hotmail.com>
2018-04-21 02:30:12 +0200

    removed softmax from last layer

--- haziq/dlc_bci.py
+++ haziq/dlc_bci.py
@@ -89,7 +89,7 @@ def train_model(model, train_input, train_target, train_mini_batch_size, test_in
     criterion = nn.CrossEntropyLoss()
     tr_loss_all = []
     te_loss_all = []
-    eta = 1e-2
+    eta = 5e-3
 
     for e in range(0, epoch):
         
