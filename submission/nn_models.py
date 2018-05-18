59b83cda158099b2fd697c198690bea9ffe5e618
HaziqRazali <ha_haziq_91@hotmail.com>
2018-05-18 12:11:07 +0200

    edit models

--- submission/nn_models.py
+++ submission/nn_models.py
@@ -1,5 +1,6 @@
 import torch
 from torch import nn
+from torch.nn import functional as F
 from torch.autograd import Variable # to initialize LSTM hidden states
 
 class ConvNet(nn.Module):
@@ -29,8 +30,8 @@ class ConvNet2(nn.Module):
     def __init__(self):
         super(ConvNet2, self).__init__()
         self.conv1 = nn.Conv1d(in_channels=28, out_channels=28, kernel_size=7)
-        self.linear1 = nn.Linear(224, 224)
-        self.output = nn.Linear(224, 2)        
+        self.linear1 = nn.Linear(308, 308)
+        self.output = nn.Linear(308, 2)        
         
     def forward(self, x, mode=False):
         
@@ -70,7 +71,7 @@ class ConvNet4(nn.Module):
 
         self.conv1 = nn.Conv2d(in_channels=1, out_channels=3, kernel_size=(1,11))
         self.conv2 = nn.Conv2d(in_channels=3, out_channels=3, kernel_size=(28,5))
-        self.linear = nn.Linear(96, 2)
+        self.linear = nn.Linear(108, 2)
         
     def forward(self, x, mode=False):
         x = self.conv1(x)
